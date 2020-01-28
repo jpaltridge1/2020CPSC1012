734c19087e674093103f4a0bb917125363023333
jpaltridge1 <59751767+jpaltridge1@users.noreply.github.com>
2020-01-27 18:42:21 -0700

    update

--- HighMidLow/HighMidLow/Program.cs
+++ HighMidLow/HighMidLow/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HighMidLow
 {
@@ -11,7 +7,7 @@ namespace HighMidLow
         static void Main(string[] args)
         {
             string num1Input, num2Input, num3Input;
-
+            string codeblock3T = "level 3T coding block";
             int num1, num2, num3, lowest, mid, highest;
 
             Console.Write("Enter Number 1: ");
@@ -27,14 +23,17 @@ namespace HighMidLow
             num3 = int.Parse(num3Input);
 
             highest = num1;
-           
+
 
             if (num2 > highest)
             {
+                //string codeblock2 = "level 2 coding block";
+
                 highest = num2;
-            
+
                 if (num3 > highest)
                 {
+                    //string codeblock2T = "level 2T coding block";
                     highest = num3;
                     mid = num2;
                     lowest = num1;
@@ -44,31 +43,37 @@ namespace HighMidLow
                 {
                     if (num3 > num1)
                     {
+                        //string codeblock3T = "level 3T coding block";
                         highest = num2;
                         mid = num3;
                         lowest = num1;
-                    }   
-                    
+                    }
+
                     else
                     {
+                        //string codeblock3F = "level 3F coding block";
                         highest = num2;
                         mid = num1;
                         lowest = num3;
+                        Console.WriteLine(codeblock3T);
                     }
                 }
-            
+
             }
             else
             {
                 if (num2 > num3)
                 {
+                    //string codeblockT2 = "level T2 coding block";
                     highest = num1;
                     mid = num2;
                     lowest = num3;
+
                 }
 
                 else
                 {
+                    //string codeblockF2 = "level F2 coding block";
                     highest = num1;
                     mid = num3;
                     lowest = num2;
